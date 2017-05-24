d888adc034f0f65997d8c3442a6ba2cee93eb440
Chaz Henricks <chazhenricks@gmail.com>
2017-05-23 20:24:08 -0500

    added basic functionality to get current weather from zip code entered

---
+++ js/calls.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function getCity(zip) {
+    return new Promise(function (resolve, reject){
+        $.ajax({
+            url: `http://maps.googleapis.com/maps/api/geocode/json?address=${zip}&sensor=true"`
+        }).done(function(data){
+            resolve(data);
+        }).fail(function(error){
+            reject(error);
+        });
+    });
+}
+
+function getWeather(city, state) {
+    return new Promise(function (resolve, reject){
+        $.ajax({
+            url: `http://api.wunderground.com/api/3018d91767478a9c/conditions/q/${state}/${city}.json`
+        }).done(function(data){
+            resolve(data);
+        }).fail(function(error){
+            reject(error);
+        });
+    });
+}
+
+
+module.exports = 
+{
+    getCity,
+    getWeather
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
 "use strict";
 
-console.log("MAIN.JS");
+let calls = require("./calls");
+let weatherTemplate = require("../templates/currentWeather.hbs");
+let Handlebars = require("hbsfy/runtime");
+
+console.log("Main.JS");
+//Submit button event
+$(document).on("click", "#submit-btn", function(){
+    let zipCode = $("#user-input").val();
+    if(checkZip(zipCode)){
+        getWeather(zipCode);
+    }else{
+         $("#user-input").val("Please Enter a Valid Zip Code");
+    }
+});
+
+//Enter keypress triggers #submit-btn event
+$(document).on("keyup", function(e){
+    if (e.keyCode === 13) {
+       $("#submit-btn").trigger("click");
+    }
+});
+
+
+//Checks to see if zip code is valid
+function checkZip(value) {
+    return (/(^\d{5}$)|(^\d{5}-\d{4}$)/).test(value);
+}
+
+
+//Inputs user zip code to get user city and then passes that city to get current weather. 
+
+function getWeather(zipcode){
+    calls.getCity(zipcode)
+    .then(function(data){
+        let city = data.results[0].address_components[1].long_name;
+        let state = data.results[0].address_components[3].short_name;
+        return calls.getWeather(city, state);
+    })
+    .then(function(data){
+        let currentWeather = data.current_observation;
+        console.log(data);
+        $("#weather-display").html(weatherTemplate(currentWeather));
+
+    });
+}
